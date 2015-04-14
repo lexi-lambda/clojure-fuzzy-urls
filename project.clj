26c5ab4ab5f39bebc20d98bf51e55e8cb69b9b3a
Alexis King <lexi.lambda@gmail.com>
2015-04-13 19:43:26 -0700

    Remove Leiningen's filler from the project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,4 @@
 (defproject fuzzy-urls "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [expectations "2.0.9"]]
   :main ^:skip-aot fuzzy-urls.example
